5c28921f83418dda127f06e2ac15df2ac4359a47
seshenya <seshenya@gmail.com>
2020-11-20 14:17:03 +0530

    changed logo location

--- src/containers/Nav.tsx
+++ src/containers/Nav.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from '../assets/logo-hpb.png';
+
+
 
 function Nav(){
     return(
         <div className="nav">
-            <img className="logo" src="../logo-hpb.png"/>
+            <img className="logo" src={logo}/>
             <div className="Nav-header">
                 <span>COVID-19 Live Situational Analysis Dashboard of Sri Lanka</span>
             </div>
